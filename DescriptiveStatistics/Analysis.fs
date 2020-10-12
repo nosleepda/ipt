be7c8a813422d3c42693e3a9e7f48b674365545e
Константин Спицин <k.spitsin@bars.group>
2020-10-13 00:03:27 +0300

    update analysis module

--- DescriptiveStatistics/Analysis.fs
+++ DescriptiveStatistics/Analysis.fs
@@ -2,17 +2,16 @@
 
 open System
 open System.Collections.Generic
+open GraphicLibrary
 open MathNet.Numerics.Distributions
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
 
 module Analysis =
     
     let Xs = [300.0 .. 100.0 .. 1000.0]
     
     let Ys = [70.35; 75.38; 80.53; 85.81; 91.26; 96.83; 102.53; 108.27]
-    
-    let dXY =
-        List.map2 (fun d f -> d, f) Xs Ys
-        |> dict
     
     let n = Xs.Length |> float
     
@@ -22,7 +21,9 @@ module Analysis =
     
     let div y = y / n
     
-    let div1 y = y / n1 
+    let div1 y = y / n1
+    
+    let div2 y = y / n2
     
     let meanX =
         List.sum Xs
@@ -66,4 +67,55 @@ module Analysis =
         let r = Math.Atanh r + (norm a) / sqrt(n - 3.0) |> tanh
         (l,r)
     
+    let emperYX =
+        let r1 = [|n; List.sum Xs; List.sum Xs; List.sumBy (fun x -> x ** 2.0) Xs|]
+        let matrix = DenseMatrix(2,2,r1)
+        let r2 = [|List.sum Ys; List.map2 (fun x y -> x * y) Xs Ys |> List.sum |] |> vector
+        matrix.Solve(r2)
     
+    let emperXY =
+        let r1 = [|n; List.sum Xs; List.sum Xs; List.sumBy (fun x -> x ** 2.0) Xs|]
+        let matrix = DenseMatrix(2,2,r1)
+        let r2 = [|List.sum Ys; List.map2 (fun x y -> x * y) Xs Ys |> List.sum |] |> vector
+        matrix.Solve(r2)
+    
+    let a = emperYX.[0]
+    
+    let b = emperYX.[1]
+    
+    let b1 =
+        r * sX / sY
+    
+    let a1 =
+        meanX - meanY * r * sX / sY
+    
+    let S = List.map2 (fun x y -> (y - a - b * x) ** 2.0) Xs Ys |> List.sum |> div2 |> sqrt
+    
+    let confidenceIntervalB =
+        let Sb = S / (sX * sqrt n1)
+        (b - t 0.05 * Sb, b + t 0.05 * Sb)
+    
+    let confidenceIntervalA =
+        let Sa = S * sqrt (1.0 / n + meanX ** 2.0 / (n1 * sX ** 2.0))
+        (a - t 0.05 * Sa, a + t 0.05 * Sa)
+    
+    let f x = a + b * x
+        
+    let R2 =
+        let temp = List.map2 (fun x y -> (y - f x) ** 2.0) Xs Ys |> List.sum
+        List.map (fun y -> (y - meanY) ** 2.0) Ys |> List.sum |> (/) temp |> (-) 1.0
+        
+    let F a =
+        let r = R2 * n2 / (1.0 - R2)
+        let l = FisherSnedecor.InvCDF(1.0,n2,1.0 - a)
+        (r,l)
+        
+    let Ys2 = List.map (fun x -> f x) Xs
+        
+    let drawCorrelationField =
+        let graphs = Graphic("Correlation field", "X", "Y")
+        graphs.SetPlane(List.min Xs - 20.0, List.max Xs + 20.0, List.min Xs - 20.0, 100.0, List.min Ys - 10.0, List.max Ys + 10.0,List.min Ys - 10.0, 10.0)
+        graphs.AddGraph(List<float> Xs, List<float> Ys,"red")
+        graphs.AddGraph(List<float> Xs, List<float> Ys2,"green")
+        graphs.DrawGraph(false)
+        
--- one/Program.fs
+++ one/Program.fs
@@ -1,9 +1,5 @@
-﻿open System
-open System.Collections.Generic
-open System.Numerics
+﻿open System.Collections.Generic
 open DescriptiveStatistics
-open MathNet.Numerics.LinearAlgebra.Double
-open XPlot.Plotly
 
 let data = List<double>[28.0; 30.0; 28.0; 27.0; 28.0; 29.0; 29.0; 29.0; 31.0; 28.0; 26.0; 25.0; 33.0;
 35.0; 27.0; 31.0; 31.0; 30.0; 28.0; 33.0; 23.0; 30.0; 31.0; 33.0; 31.0; 27.0;
@@ -19,17 +15,43 @@ let data = List<double>[28.0; 30.0; 28.0; 27.0; 28.0; 29.0; 29.0; 29.0; 31.0; 28
 [<EntryPoint>]
 let main argv =
     
-    let r1 = [|8.0; 5200.0; 5200.0; 3800000.0|]
-    let r2 = [|710.96; 484901.0|] |> System.Numerics.Vector
+    Statistics.mode |> printfn "mode %f"
+    Statistics.m3 |> printfn "m3 %f"
+    Statistics.initElem |> printfn "initElem %f"
+    Statistics.lastElem |> printfn "lastElem %f"
+    Statistics.median |> printfn "median %f"
+    Statistics.skewness |> printfn "skewness %f"
+    Statistics.kurtosis|> printfn "kurtosis %f"
+    Statistics.coefficientVariation |> printfn "cv %f"
+    Statistics.mean |> printfn "mxG %f"
+    Statistics.variance |> printfn "dxG %f"
+    Statistics.standardDeviation |> printfn "sG %f"
+    Statistics.confidenceIntervalMean 0.95 ||> printfn "chi %f < s < %f"
+    Statistics.confidenceIntervalVariance 0.05 ||> printfn "CI %f < m < %f"
+    Statistics.nNoGroup |> printfn "n %i\n"
     
-//    let m1 = array2D r1
-    let m2 = DenseMatrix(2,2,r1)
+    printfn "\nStatistics.discrete" |> ignore
+    Statistics.discrete |> List.iter (printf " %1.6f ")
+    printfn "\nStatistics.frequenciesDiscrete" |> ignore
+    Statistics.frequenciesDiscrete |> List.ofSeq |> List.iter (printf "%i ")
+    printfn "\nHypothesis.frequenciesDiscreteShort" |> ignore
     
-   
+    Hypothesis.frequenciesDiscreteShort |> List.ofSeq |> List.iter (printf "%i ")
+    printfn "\nHypothesis.intervalShort" |> ignore
+    Hypothesis.intervalShort |> List.ofSeq |> List.iter (printf "%f ")
+//    printfn "" |> ignore
+//    Hypothesis.isShort.IsSome |> (printf "sss %A ")
+    printfn "" |> ignore
+    Hypothesis.chiSquared 0.95 |> printfn "%s"
+    Hypothesis.kolmogorov 0.05 |> printfn "%s"
+    Hypothesis.romanovsky |> printfn "%s" 
+    Hypothesis.yastremsky |> printfn "%s"
+    Hypothesis.approximate 0.95 |> printfn "%s"
     
-    let s = Analysis.arth 0.05
-    
-//    printfn "%A" (m2.Solve(r2))
+//    printfn "%A" (Analysis.F 0.05)
+//    printfn "%A" Analysis.b
+//    printfn "%A" Analysis.a1
+//    printfn "%A" Analysis.b1
 
-//    
+    
     0
